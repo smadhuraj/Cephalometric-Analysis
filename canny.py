c7dc14e2b2fe97305412d166588c4fde97586241
shashintha <shashimadhuraj@gmail.com>
2019-01-18 01:38:42 +0530

    update

--- canny.py
+++ canny.py
@@ -6,6 +6,7 @@ from matplotlib import pyplot as plt
 
 
 img = cv2.imread('scull.jpg',0)# read the X ray image in gray scale
+img_1 = cv2.imread('scull.jpg',1)
 edges = cv2.Canny(img,100,200) # apply Canny filter to loded image
 
 height = np.size(img, 0)# get height of the image
@@ -63,32 +64,53 @@ for i in range(1,height):# to find pogonian Y coodinate go top to buttom through
 
 cv2.circle(edges,(y, k), 5, (255,255,255), -1)##draw the cricle on the skul and on the POG point
 #############################################
+#find N point of the skull (top of the nose)
+def findNPoint(edges, x_2, y_2):
+	test_point = y_2
+	return_point_x = 0
+	return_point_y = 0
+	for i in range(x_2, height-1):
+		for j in range(test_point-3, test_point+3):
+			if edges[i+1][j] == 255:
+				test_point = j
+				# print(j)
+				break
+				
+			if j == test_point + 2 and edges[i+1][j] == 0:
+				return_point_y = j-3
+				return_point_x = i-1
+				break
 
-test_point = y_2
-
-return_point_x = 0
-return_point_y = 0
-
-for i in range(x_2, height-1):
-	for j in range(test_point-3, test_point+3):
-		if edges[i+1][j] == 255:
-			test_point = j
-			print(j)
+		if return_point_x != 0:
 			break
-			
-		if j == test_point + 3 and edges[i+1][j] == 0:
-			return_point_y = j-3
-			return_point_x = i-1
-			break
+	return return_point_x, return_point_y
+###########################################
 
-	if return_point_x != 0:
-		break
+N_x, N_y = findNPoint(edges,x_2,y_2)#call the find Npoint function
 
+cv2.circle(edges,(N_y, N_x), 5, (255,255,255), -1)#draw circle on N point
+cv2.line(img_1, (y, k), (N_y, N_x), (0, 0, 255), thickness=1, lineType=8)#draw line between POG and N
+############################################
 
-cv2.circle(edges,(return_point_y, return_point_x), 5, (255,255,255), -1)
+h = int(height/2)
+w = int(width/2)
+a = np.zeros([h, w]) 
 
-cv2.imshow('img',edges )# show canny filtered image
-cv2.imshow('img_1',img )# show input image (X ray image)
+Matrix_4 = np.zeros(a.shape)
+
+Matrix_4_lap = np.zeros(a.shape)
+
+for i in range(1,height-1):
+	for j in range(1,width-1):
+		if i>h and j>w:
+			Matrix_4[i-h][j-w] = edges[i][j]
+			Matrix_4_lap[i-h][j-w] = img[i][j] # that has an error!!!!!!!!!!!!!!!!!!!!!!
+
+cv2.imshow('img_test',Matrix_4)
+cv2.imshow('img_test_1',Matrix_4_lap)
+
+cv2.imshow('img',edges)# show canny filtered image
+cv2.imshow('img_1',img_1)# show input image (X ray image)
 # cv2.imshow('scull image',edges)
 # cv2.imshow('scull image_rotate',rotate_img)
 cv2.waitKey(0)
--- project/README.txt
+++
@@ -1,2 +0,0 @@
-sample_1.png file size is 60X60 pixel
-canny.py is used to automate system for analysis the skul and get 4 point acuratly
